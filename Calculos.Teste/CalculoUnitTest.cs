de52243b89f1691e88fb3976eccd8ae23a5450f7
Henrique <henriquebayer8@gmail.com>
2024-08-02 08:41:32 -0300

    .

---
+++ Calculos.Teste/CalculoUnitTest.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculos.Teste
+{
+    public class CalculoUnitTest
+    {
+
+        [Fact]
+        public void Test1()
+        {
+
+            var x1 = 10;
+            var x2 = 5;
+            var valorEsperadoS = 15;
+          
+            //Act
+
+            var soma = Class1.Somar(x1, x2);
+          
+
+            //Assert - Provar
+            Assert.Equal(valorEsperadoS, soma);
+            
+
+        }
+
+
+        [Fact]
+        public void Test2()
+        {
+
+            var x1 = 10;
+            var x2 = 5;
+           
+            var valorEsperadoSu = 5;
+            
+
+            //Act
+
+           
+            var subtracao = Class1.Subtracao(x1, x2);
+            
+            //Assert - Provar
+          
+            Assert.Equal(valorEsperadoSu, subtracao);
+           
+        }
+
+
+        [Fact]
+        public void Test3()
+        {
+
+            var x1 = 10;
+            var x2 = 5;
+           
+            var valorEsperadoD = 2;
+          
+            //Act
+
+           
+            var divisao = Class1.Divisao(x1, x2);
+            
+
+            //Assert - Provar
+           
+            Assert.Equal(valorEsperadoD, divisao);
+         
+
+        }
+
+
+        [Fact]
+        public void Test4()
+        {
+
+            var x1 = 10;
+            var x2 = 5;
+            
+            var valorEsperadoM = 50;
+
+            //Act
+
+           
+            var multiplicacao = Class1.Multiplicacao(x1, x2);
+
+            //Assert - Provar
+          
+            Assert.Equal(valorEsperadoM, multiplicacao);
+
+        }
+
+
+        [Fact]
+        public void VerifyEmail()
+        {
+            string email = "henrique@gmail.com";
+
+            Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+            Match match = regex.Match(email);
+
+            Assert.True(match.Success, email);
+        }
+
+
+
+        [Fact]
+        public void TestCelciusFareight()
+        {
+
+            var c = 30;
+           
+          var result =  Class1.CelciusFareight(c);
+
+            var valorEsperadoD = 86;
+
+          
+
+
+            //Assert - Provar
+
+            Assert.Equal(valorEsperadoD, result);
+
+
+        }
+    }
+}
---
+++ Calculos/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculos
+{
+    public static class Class1
+    {
+
+        public static double Somar(double x , double y)
+        {
+
+            return x + y;
+
+        }
+        public static double Subtracao(double x, double y)
+        {
+
+            return x - y;
+
+        }
+
+        public static double Divisao(double x, double y)
+        {
+
+            return x/y;
+
+        }
+
+        public static double Multiplicacao(double x, double y)
+        {
+
+            return x * y;
+
+        }
+
+
+        public static void AddLivro()
+        {
+            Console.WriteLine("Digite o livro que quer colocar na estante!");
+
+            Console.WriteLine("Nome do Livro:");
+            string x = Console.ReadLine();
+
+
+            List<string> Estante = new List<string>();
+
+            Estante.Add(x);
+
+
+            for (int i = 0; i < Estante.Count(); i++)
+            {
+                Console.WriteLine("A estante esta com os livros:" + Estante[0 + i]);
+            }
+
+
+
+
+
+        }
+
+        public static double CelciusFareight(double c)
+        {
+;
+
+           return c * 1.8 + 32;
+        }
+
+        public static string DigitEmail()
+        {
+            Console.WriteLine("Digite o seu Email!");
+
+            Console.WriteLine("Email:");
+            string email = Console.ReadLine();
+
+            return email;
+        }
+    }
+}
